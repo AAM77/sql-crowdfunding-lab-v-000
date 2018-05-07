0f4fdb6d9e3542d20ebb93608628371c7141e387
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-07 16:01:39 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,6 +42,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT category, SUM(amount) FROM projects
  INNER JOIN pledges
  ON projects.id = pledges.project_id
+ GROUP BY projects.id
  HAVING category = 'music';"
 end
 
